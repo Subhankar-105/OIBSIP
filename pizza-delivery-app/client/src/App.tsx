bba8c7be46ce839cbc6f182d72ab5151b152b056
Hi-mang-shu <official.for.subhankar111@gmail.com>
2026-02-26 09:40:52 +0530

    Frontend setup ready

---
+++ pizza-delivery-app/client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-green-600">
+        Pizza App with TypeScript + Tailwind 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
